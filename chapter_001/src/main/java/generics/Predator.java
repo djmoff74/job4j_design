d418f7356e54acfb1651fcd860eadd27c094fdf1
djmoff74 <djmoff74@gmail.com>
2020-12-23 01:13:27 +0500

    0. Видео[4952#263601]

---
+++ chapter_001/src/main/java/generics/Predator.java
@@ -0,0 +1,12 @@
+package generics;
+
+/**
+ * Class Predator
+ *
+ * @author Eduard Lykov (djmoff74@gmail.com)
+ * @version 1.0
+ * @since 23.12.2020
+ */
+public class Predator extends Animal {
+
+}
